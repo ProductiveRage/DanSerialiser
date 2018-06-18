f5d41d15335c8a3be5b37a14b536da45751112d7
ProductiveRage <dangger36@gmail.com>
2018-06-19 00:44:08 +0100

    Added an analyser to ensure use of [Deprecated] matches assumptions in serialisation process
    The anayser project needs to target netstandard1.3 in order to be loaded
    in VS2017 as an analyser (targeting netstandard2.0 will result in an
    IDE1003 warning in the VS project that references the analyser, saying
    that the assembly "depends on 'netstandard, Version=2.0.0.0,
    Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51' but it was not found.
    Analyzers may not run correctly unless the missing assembly is added as
    an analyzer reference as well.") - see
    https://github.com/dotnet/roslyn/issues/22368

---
+++ DanSerialiserAnalyser.UnitTests/DeprecatedAnalyserTests.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using TestHelper;
+using Xunit;
+
+namespace DanSerialiserAnalyser.UnitTests
+{
+	public class DeprecatedAnalyserTests : DiagnosticVerifier
+	{
+		[Fact]
+		public void IdealUsageForTargetProperty()
+		{
+			var testContent = @"
+				using DanSerialiser;
+
+				namespace TestCase
+				{
+					public class MyClass
+					{
+						[Deprecated(replacedBy: nameof(Something)]
+						public int ID { get { return Something; } set { Something = value; } }
+
+						public int Something { get; }
+					}
+				}";
+
+			VerifyCSharpDiagnostic(testContent);
+		}
+
+		[Fact]
+		public void IdealUsageForTargetField()
+		{
+			var testContent = @"
+				using DanSerialiser;
+
+				namespace TestCase
+				{
+					public class MyClass
+					{
+						[Deprecated(replacedBy: nameof(Something)]
+						public int ID { get { return Something; } set { Something = value; } }
+
+						public int Something;
+					}
+				}";
+
+			VerifyCSharpDiagnostic(testContent);
+		}
+
+		[Fact]
+		public void NoArguments()
+		{
+			var testContent = @"
+				using DanSerialiser;
+
+				namespace TestCase
+				{
+					public class MyClass
+					{
+						[Deprecated]
+						public int ID { get { return Something; } }
+
+						public int Something { get; }
+					}
+				}";
+
+			VerifyCSharpDiagnostic(testContent);
+		}
+
+		[Fact]
+		public void NoArgumentsWithBrackets()
+		{
+			var testContent = @"
+				using DanSerialiser;
+
+				namespace TestCase
+				{
+					public class MyClass
+					{
+						[Deprecated()]
+						public int ID { get { return Something; } }
+
+						public int Something { get; }
+					}
+				}";
+
+			VerifyCSharpDiagnostic(testContent);
+		}
+
+		/// <summary>
+		/// It could be argued that this approach is valid but using nameof(..) is better because it's more refactor friendly and so the analyser requires nameof
+		/// </summary>
+		[Fact]
+		public void SpecifyingThePropertyByStringIsNotOk()
+		{
+			var testContent = @"
+				using DanSerialiser;
+
+				namespace TestCase
+				{
+					public class MyClass
+					{
+						[Deprecated(replacedBy: ""Something"")]
+						public int ID { get { return Something; } set { Something = value; } }
+
+						public int Something { get; }
+					}
+				}";
+
+			var expected = new DiagnosticResult
+			{
+				Id = DeprecatedAnalyser.DiagnosticId,
+				Message = string.Format(DeprecatedAnalyser.ReplacedByMustBeNameofPropertyRule.MessageFormat.ToString()),
+				Severity = DiagnosticSeverity.Error,
+				Locations = new[]
+				{
+					new DiagnosticResultLocation("Test0.cs", 8, 8)
+				}
+			};
+
+			VerifyCSharpDiagnostic(testContent, expected);
+		}
+
+		[Fact]
+		public void ReplacedByNameofTargetMustBeProperty()
+		{
+			var testContent = @"
+				using DanSerialiser;
+
+				namespace TestCase
+				{
+					public class MyClass
+					{
+						[Deprecated(replacedBy: nameof(MyClass)]
+						public int ID { get { return Something; } set { Something = value; } }
+
+						public int Something { get; }
+					}
+				}";
+
+			var expected = new DiagnosticResult
+			{
+				Id = DeprecatedAnalyser.DiagnosticId,
+				Message = string.Format(DeprecatedAnalyser.ReplacedByMustBeNameofPropertyRule.MessageFormat.ToString()),
+				Severity = DiagnosticSeverity.Error,
+				Locations = new[]
+				{
+					new DiagnosticResultLocation("Test0.cs", 8, 8)
+				}
+			};
+
+			VerifyCSharpDiagnostic(testContent, expected);
+		}
+
+		/// <summary>
+		/// It keeps everything simpler if the target property must be declared on the same class (this restriction might be relaxed in the future but the serialisation process assumes 
+		/// it for now and so it's best to check for it)
+		/// </summary>
+		[Fact]
+		public void ReplacedByNameofTargetMustBePropertyOnSameClass()
+		{
+			var testContent = @"
+				using DanSerialiser;
+
+				namespace TestCase
+				{
+					public class MyClass : MyBaseClass
+					{
+						[Deprecated(replacedBy: nameof(Something)]
+						public int ID { get { return Something; } set { Something = value; } }
+					}
+
+					public class MyBaseClass
+					{
+						public int Something { get; }
+					}
+				}";
+
+			var expected = new DiagnosticResult
+			{
+				Id = DeprecatedAnalyser.DiagnosticId,
+				Message = string.Format(DeprecatedAnalyser.ReplacedByMustBeNameofPropertyRule.MessageFormat.ToString()),
+				Severity = DiagnosticSeverity.Error,
+				Locations = new[]
+				{
+					new DiagnosticResultLocation("Test0.cs", 8, 8)
+				}
+			};
+
+			VerifyCSharpDiagnostic(testContent, expected);
+		}
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+		{
+			return new DeprecatedAnalyser();
+		}
+	}
+}
---
+++ DanSerialiserAnalyser/DeprecatedAnalyser.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace DanSerialiserAnalyser
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public sealed class DeprecatedAnalyser : DiagnosticAnalyzer
+	{
+		public const string DiagnosticId = "DanSer001";
+		private const string Category = "Design";
+		public static DiagnosticDescriptor ReplacedByMustBeNameofPropertyRule = new DiagnosticDescriptor(
+			DiagnosticId,
+			"Deprecated Analyser",
+			"The 'replacedBy' argument on a [Deprecated] attribute must specify a field or property defined within the same class and it must do so using nameof",
+			Category,
+			DiagnosticSeverity.Error,
+			isEnabledByDefault: true
+		);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+		{
+			get
+			{
+				return ImmutableArray.Create(ReplacedByMustBeNameofPropertyRule);
+			}
+		}
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.RegisterSyntaxNodeAction(LookForFieldWithMultipleDeprecatedAttributes, SyntaxKind.Attribute);
+		}
+
+		private void LookForFieldWithMultipleDeprecatedAttributes(SyntaxNodeAnalysisContext context)
+		{
+			if (!(context.Node is AttributeSyntax attribute))
+				return;
+
+			var ownerOfAttributes = ((attribute.Parent as AttributeListSyntax)?.Parent as MemberDeclarationSyntax);
+			SyntaxList<AttributeListSyntax> attributeLists;
+			if (ownerOfAttributes is BaseFieldDeclarationSyntax field)
+				attributeLists = field.AttributeLists;
+			else if (ownerOfAttributes is BasePropertyDeclarationSyntax property)
+				attributeLists = property.AttributeLists;
+			else
+				return;
+
+			var allAttributes = attributeLists.SelectMany(a => a.Attributes);
+			var deprecatedAttributesWithSingleArgument = allAttributes
+				.Where(a => (a.ArgumentList != null) && (a.ArgumentList.Arguments.Count == 1))
+				.Select(a =>
+				{
+					if (a.Name is IdentifierNameSyntax identifier)
+						return new { Attribute = a, identifier.Identifier };
+					else if (a.Name is QualifiedNameSyntax qualifierIdentifier)
+						return new { Attribute = a, qualifierIdentifier.Right.Identifier };
+					else
+						return null;
+				})
+				.Where(a => a != null)
+				.Where(a => (a.Identifier.Text == "Deprecated") || (a.Identifier.Text == "DeprecatedAttribute"))
+				.Select(a => a.Attribute);
+			if (!deprecatedAttributesWithSingleArgument.Any())
+				return;
+
+			foreach (var deprecatedAttribute in deprecatedAttributesWithSingleArgument)
+			{
+				// Ensure that this is the [Deprecated] attribute from DanSerialiser and not one from somewhere else (if can't resolve it then presume that there is
+				// a problem with the code and wait until it all compiles properly)
+				// Note: Don't need to check that argument name since [Deprecated] only has one ctor signature that takes a single argument (which is "replacedBy")
+				if ((context.SemanticModel.GetSymbolInfo(deprecatedAttribute).Symbol as IMethodSymbol)?.ContainingNamespace?.Name != "DanSerialiser")
+					continue;
+
+				// The "replacedBy" value must indicate a Field or Property. Technically, it would be ok to specify the name with a string literal or with a string
+				// constant but the best way is to use nameof(..) since that's more refactor-friendly and so that's the only way that I'm going to accept.
+				var argumentExpression = deprecatedAttribute.ArgumentList.Arguments[0].Expression;
+				if (((argumentExpression is InvocationExpressionSyntax invocation))
+				&& (invocation.Expression is IdentifierNameSyntax invocationTargetName)
+				&& (invocationTargetName.Identifier.Text == "nameof")
+				&& (invocation.ArgumentList != null)
+				&& (invocation.ArgumentList.Arguments != null)
+				&& (invocation.ArgumentList.Arguments.Count == 1))
+				{
+					var nameOfTargetSymbol = context.SemanticModel.GetSymbolInfo(invocation.ArgumentList.Arguments[0].Expression).Symbol;
+					if (nameOfTargetSymbol != null)
+					{
+						ISymbol target;
+						if ((nameOfTargetSymbol is IPropertySymbol targetProperty) && !targetProperty.Parameters.Any())
+							target = targetProperty;
+						else if (nameOfTargetSymbol is IFieldSymbol fieldProperty)
+							target = fieldProperty;
+						else
+							target = null;
+						if (target != null)
+						{
+							// For now, the BinarySerialisationReader presumes that the 'replacedBy' target will be declared on the same class as the property that is
+							// [Deprecated] and so we need to check for that (this might be relaxed in the future so that the target can be elsewhere, such as on a
+							// base class, but since that assumption is currently in play it's best to test for it)
+							if (target.ContainingType == null)
+							{
+								// If can't resolve the property's ContainingType then presume that the code is invalid and leave worrying about this until the code compiles
+								continue;
+							}
+							var ownerSymbol = context.SemanticModel.GetDeclaredSymbol(ownerOfAttributes);
+							if ((ownerSymbol == null) || (ownerSymbol.ContainingType == null))
+							{
+								// If can't resolve the owner reference then presume that the code is invalid
+								return;
+							}
+							if (ownerSymbol.ContainingType == target.ContainingType)
+								continue;
+						}
+					}
+				}
+
+				context.ReportDiagnostic(Diagnostic.Create(
+					ReplacedByMustBeNameofPropertyRule,
+					context.Node.GetLocation()
+				));
+			}
+		}
+	}
+}
