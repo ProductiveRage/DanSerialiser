79f85fb577e51fc9d15e4db2ba9114ce0de8b2b9
ProductiveRage <dangger36@gmail.com>
2018-10-10 23:23:11 +0100

    Added FastestTreeBinarySerialisation class to expose binary serialisation using the SpeedyButLimited reference reuse option

--- DanSerialiser/Attributes/SpecialisationsMayBeIgnoredWhenSerialisingAttribute.cs
+++ DanSerialiser/Attributes/SpecialisationsMayBeIgnoredWhenSerialisingAttribute.cs
@@ -2,22 +2,16 @@
 
 namespace DanSerialiser
 {
-	// ================================================================================================================================================================================
-	// TODO
-	//  - Analyser for this (and tests for analyser)
-	//  - Update summary to provide example and counter-example of where this attribute may apply
-	//    > Need  to expose SpeedyButLimited in order to do that!
-	// ================================================================================================================================================================================
-
 	/// <summary>
 	/// This may be applied to a field or property on a type that will be serialised to indicate that references assigned to that field or property will always match the field / property
 	/// type precisely and never be a more specialised type (for example, if the property type is MyList and if MyList is a non-sealed class then this attribute may be added to that
 	/// property to tell the serialisation process that the value will ALWAYS be an instance of MyList or null - and not an instance of another type that inherits from MyList). It also
 	/// has the effect of saying that any fields or properties on the specified type that are interfaces or abstract types are optional and that it is acceptable for the serialiser to
 	/// write away null values for them. It should be noted, though, that this attribute is not guaranteed to affect serialisation behaviour but that it opens the door for serialisers
-	/// to perform more aggressive optimisations if the serialiser is configured to do so. It might be applied, for example, to a property of type Dictionary&lt;int, string&gt; if it
-	/// is known that the property will only ever be assigned a value of that type (and not a more specialised type that is derived from it) AND if values will never have a custom
-	/// equality comparer specified.
+	/// to perform more aggressive optimisations if the serialiser is configured to do so (currently, only the FastestTreeBinarySerialisation methods will take advantage of this attribute).
+	/// 
+	/// It might be applied, for example, to a property of type Dictionary&lt;int, string&gt; if it is known that the property will only ever be assigned a value of that type (and not a
+	/// more specialised type that is derived from it) AND if values will never have a custom equality comparer specified.
 	/// 
 	/// This attribute should only be applied to fields or properties whose types are non-sealed, non-abstract classes - it does not make sense for primitives or value types or sealed
 	/// classes (because they have no opportunity to be derived into specialised types) and it does not makes sense for abstract classes or interfaces (because they require derived
---
+++ DanSerialiser/FastestTreeBinarySerialisation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace DanSerialiser
+{
+	public static class FastestTreeBinarySerialisation
+	{
+		/// <summary>
+		/// This uses the BinarySerialisationWriter (and a MemoryStream) in a configuration that disables reference reuse and circular reference tracking, which enables additional
+		/// optimisations to be made to the process for faster serialisation. Not only is it limited in terms of reference tracking but it also does not allow for any serialisation
+		/// type converters to be specified. This serialisation method should not be used with data in which the same references appear multiple times - if there are any circular
+		/// references then a stack overflow exception will be thrown and if the same references are repeated many times within the data then the standard Binary Serialisation
+		/// Writer may provide the best performance - but it may offer the best serialisation performance if your data structure meets these requirements. It still has support
+		/// for the DanSerialiser attributes that affect entity versioning (such as Deprecated and OptionalWhenDeserialising) and its output may still be consumed by the
+		/// BinarySerialisationReader. Note: Using sealed classes wherever possible allows this serialisation process to enable more optimisations in some cases
+		/// and so that is a recommended practice if you want the fastest results.
+		/// </summary>
+		public static byte[] Serialise(object value)
+		{
+			using (var stream = new MemoryStream())
+			{
+				var writer = GetSerialisationWriter(stream);
+				Serialiser.Instance.Serialise(value, new ISerialisationTypeConverter[0], writer);
+				return stream.ToArray();
+			}
+		}
+
+		/// <summary>
+		/// This may be used if you wish to perform the serialisation on a particular stream, rather than having to generate a byte array. The same notes apply as to the 'Serialise'
+		/// method in this class - this should offer the most efficient binary serialisation that this library has to offer, so long as your data meets the requirements outlined in
+		/// that method's summary comment.
+		/// </summary>
+		public static IWrite GetSerialisationWriter(Stream stream)
+		{
+			if (stream == null)
+				throw new ArgumentNullException(nameof(stream));
+
+			return new BinarySerialisationWriter(stream, ReferenceReuseOptions.SpeedyButLimited);
+		}
+	}
+}
