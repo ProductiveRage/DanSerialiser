aa126a7c85d0dc866b9085ca1254fcb2f61076bc
ProductiveRage <dangger36@gmail.com>
2018-10-12 23:26:18 +0100

    Started on ImmutableListTypeConverter tests

---
+++ UnitTests/ImmutableListTypeConverterTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Immutable;
+using DanSerialiser;
+using Xunit;
+
+namespace UnitTests
+{
+	public static class ImmutableListTypeConverterTests
+	{
+		public static class MicrosoftImmutableList
+		{
+			[Fact]
+			public static void NullImmutableListOfStringSerialisedAsNull()
+			{
+				ImmutableList<string> value = null;
+				var serialised = ((ISerialisationTypeConverter)ImmutableListTypeConverter.Instance).ConvertIfRequired(value);
+				AssertEqualContentsAndThatTypesMatch(null, serialised);
+
+				var deserialised = ((IDeserialisationTypeConverter)ImmutableListTypeConverter.Instance).ConvertIfRequired(
+					typeof(ImmutableList<string>),
+					serialised
+				);
+				AssertEqualContentsAndThatTypesMatch(value, deserialised);
+			}
+
+			[Fact]
+			public static void ImmutableListOfStringSerialisedViaStringArray()
+			{
+				var value = ImmutableList.Create("One", "Two");
+				var serialised = ((ISerialisationTypeConverter)ImmutableListTypeConverter.Instance).ConvertIfRequired(value);
+				AssertEqualContentsAndThatTypesMatch(new[] { "One", "Two" }, serialised);
+
+				var deserialised = ((IDeserialisationTypeConverter)ImmutableListTypeConverter.Instance).ConvertIfRequired(
+					typeof(ImmutableList<string>),
+					serialised
+				);
+				AssertEqualContentsAndThatTypesMatch(value, deserialised);
+			}
+		}
+
+		private static void AssertEqualContentsAndThatTypesMatch(object expected, object actual)
+		{
+			if (expected == null)
+			{
+				Assert.Null(actual);
+				return;
+			}
+			else
+				Assert.NotNull(actual);
+
+			Assert.IsType(expected.GetType(), actual);
+			Assert.Equal(expected, actual); // For collection types, this compares their contents but not they type (hence the code above)
+		}
+	}
+}
