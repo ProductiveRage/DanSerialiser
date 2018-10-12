25b76a03c55e24db73d08a7745ac2b0c7f86d81e
ProductiveRage <dangger36@gmail.com>
2018-10-12 23:09:30 +0100

    Add a couple of tests for the EnumAsStringTypeConverter

---
+++ UnitTests/EnumAsStringTypeConverterTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DanSerialiser;
+using Xunit;
+
+namespace UnitTests
+{
+	public static class EnumAsStringTypeConverterTests
+	{
+		[Fact]
+		public static void UnrecognisedValueWillBeDeserialisedIntoEnumDefault()
+		{
+			Assert.Equal(
+				(DayOfWeek)0,
+				((IDeserialisationTypeConverter)EnumAsStringTypeConverter.Instance).ConvertIfRequired(
+					typeof(DayOfWeek),
+					"Unknown"
+				)
+			);
+		}
+
+		[Fact]
+		public static void RoundTripOfNonZeroEnumValueWorks()
+		{
+			var value = DayOfWeek.Monday; // Monday has value 1
+			var serialised = ((ISerialisationTypeConverter)EnumAsStringTypeConverter.Instance).ConvertIfRequired(value);
+			Assert.Equal(
+				value,
+				((IDeserialisationTypeConverter)EnumAsStringTypeConverter.Instance).ConvertIfRequired(
+					value.GetType(),
+					serialised
+				)
+			);
+		}
+	}
+}
