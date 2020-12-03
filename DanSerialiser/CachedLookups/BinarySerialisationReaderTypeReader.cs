cafc8f7a0cea5ffe947f1064ea2035a34f5a6779
Dan Roberts <dangger36@gmail.com>
2020-12-03 22:09:00 +0000

    When deserialising multiple instances of the same type in a single Deserialise call, if the serialised data had additional fields in (eg. if a newer version of an entity had been passed over the wire to where an assembly with an older version of the entity type was being used as the deserialisation target) an exception "Fields appeared out of order while being processed by BinarySerialisationReaderTypeReader" would incorrectly be raised

--- DanSerialiser/CachedLookups/BinarySerialisationReaderTypeReader.cs
+++ DanSerialiser/CachedLookups/BinarySerialisationReaderTypeReader.cs
@@ -25,16 +25,33 @@ namespace DanSerialiser.CachedLookups
 			if (reader == null)
 				throw new ArgumentNullException(nameof(reader));
 
-			foreach (var field in _fields)
+			var numberOfFieldsSet = 0;
+			while (numberOfFieldsSet < _fields.Length)
 			{
+				var field = _fields[numberOfFieldsSet];
 				if (nextEntryType != BinarySerialisationDataType.FieldName)
 					throw new InvalidSerialisationDataFormatException("Unexpected data type encountered while processing fields in BinarySerialisationReaderTypeReader: " + nextEntryType);
-				if (reader.ReadNextNameReferenceID(reader.ReadNextDataType()) != field.FieldNameReferenceID)
-					throw new InvalidSerialisationDataFormatException($"Fields appeared out of order while being processed by BinarySerialisationReaderTypeReader");
 
-				var value = reader.Read(ignoreAnyInvalidTypes, field.FieldType);
-				foreach (var setter in field.Setters)
-					setter(ref instance, value);
+				if (reader.ReadNextNameReferenceID(reader.ReadNextDataType()) != field.FieldNameReferenceID)
+				{
+					// 2020-12-03 DWR: If this isn't the FieldNameReferenceID that we were expecting next then it must be a field that doesn't exist on the source type in this version of the assembly, suggesting that we're deserialising
+					// data from a different version of the type that has this additional field. We want to just skip over this data. We know that we won't end up missing out setting any fields that we DO want on the current version of
+					// the entity because the while loop we're in would try to read past the end of the data and it would throw.
+					reader.Read(ignoreAnyInvalidTypes: true, targetTypeIfAvailable: null);
+				}
+				else
+				{
+					var value = reader.Read(ignoreAnyInvalidTypes, field.FieldType);
+					foreach (var setter in field.Setters)
+						setter(ref instance, value);
+					numberOfFieldsSet++;
+				}
+
+				nextEntryType = reader.ReadNextDataType();
+			}
+			while (nextEntryType == BinarySerialisationDataType.FieldName) // 2020-12-03 DWR: This corresponds with the change above - if there are new properties AFTER all of the expected fields then we need to skip over those as well
+			{
+				reader.Read(ignoreAnyInvalidTypes: true, targetTypeIfAvailable: null);
 				nextEntryType = reader.ReadNextDataType();
 			}
 			if (nextEntryType != BinarySerialisationDataType.ObjectEnd)
